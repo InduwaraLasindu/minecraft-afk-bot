4970ec73602db6b3fbcdf9013b9951eda30810cd
InduwaraLasindu <109286038+InduwaraLasindu@users.noreply.github.com>
2026-05-12 01:06:03 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,22 +4,27 @@ function createBot() {
   const bot = mineflayer.createBot({
     host: 'louvregrade12.aternos.me',
     port: 29642,
-    username: 'AFK_Bot'
+    username: 'AFK_Bot',
+    version: '1.21.1'
+  })
+
+  bot.on('login', () => {
+    console.log('Bot logged in!')
   })
 
   bot.on('spawn', () => {
-    console.log('Bot joined!')
+    console.log('Bot spawned!')
 
     setInterval(() => {
-      bot.setControlState('jump', true)
-
-      setTimeout(() => {
-        bot.setControlState('jump', false)
-      }, 500)
+      bot.swingArm()
     }, 30000)
   })
 
+  bot.on('kicked', console.log)
+  bot.on('error', console.log)
+
   bot.on('end', () => {
+    console.log('Disconnected! Reconnecting...')
     setTimeout(createBot, 10000)
   })
 }
